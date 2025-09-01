d4955587d1bf36ac21cf21f76b6ae19e3a44702c
Kellvin Correia Alves <kellvin.correia@gmail.com>
2025-09-01 20:57:31 -0300

    update task 1

--- src/Main.java
+++ src/Main.java
@@ -9,33 +9,29 @@ void main(String[] args) {
     System.out.println("Agora insira os valores de N: " + numeroN + " em sequencia, são " + numeroN * numeroN + " números");
     int[][] matriz = new int[numeroN][numeroN];
 
-    int numUnder0[] = new int[numeroN * numeroN];
-
-    for (int i = 0; i < numeroN; i++) {
-        for (int j = 0; j < numeroN; j++) {
-            System.out.println("Insira o numero da matriz: ");
+    System.out.println("Insira o numero da matriz: ");
+    for (int i = 0; i < matriz.length; i++) {
+        for (int j = 0; j < matriz[i].length; j++) {
             matriz[i][j] = scanner.nextInt();
         }
     }
-    for (int i = 0; i < numeroN; i++) {
+    for (int i = 0; i < matriz.length; i++) {
         System.out.println();
-        for (int j = 0; j < numeroN; j++) {
-            System.out.printf(String.valueOf(matriz[i][j]));
+        for (int j = 0; j < matriz[i].length; j++) {
+            System.out.printf(String.valueOf(matriz[i][j])+" ");
         }
     }
     System.out.println();
-    for (int i = 0; i < numeroN; i++) {
-        System.out.println("Diagonal principal" + String.valueOf(matriz[i][i]));
+    for (int i = 0; i < matriz.length; i++) {
+        System.out.println("Diagonal principal " + String.valueOf(matriz[i][i]));
     }
     System.out.println();
-    for (int i = 0; i < numeroN; i++) {
-        for (int j = 0; j < numeroN; j++) {
+    for (int i = 0; i < matriz.length; i++) {
+        for (int j = 0; j < matriz[i].length; j++) {
             if (matriz[i][j] < 0) {
                 System.out.println("Numero negativo encontrado: " + String.valueOf(matriz[i][j]));
             }
         }
     }
-
-
     scanner.close();
 }
